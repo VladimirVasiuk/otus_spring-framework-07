9963d1cc751547c7012c45f741e898d850b77354
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-07-17 22:24:33 +0600

    -2

--- src/main/java/ru/otus/spring06/domain/Email.java
+++ src/main/java/ru/otus/spring06/domain/Email.java
@@ -4,11 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
 public class Email {
 
-    @Id
-    @GeneratedValue
     private int id;
 
     private String email;
